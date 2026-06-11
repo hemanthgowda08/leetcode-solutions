cdb877bbda0c4cd84d34edb62738ff35c65212ae
hemanthgowda08 <hemanthkriss007@gmail.com>
2026-06-12 00:01:27 +0530

    Time: 26 ms (8.34%) | Memory: 12.2 MB (87.22%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def reverse(self, x):
+        rev = 0
+        sign = 1
+        if x < 0:
+            sign = -1
+            x = -x
+
+
+        while x != 0:
+            d = x % 10
+            rev = rev * 10 + d
+            x = x // 10
+
+        if rev < -2**31 or rev > 2**31 - 1:
+            return 0
+
+        
+
+        return rev * sign
