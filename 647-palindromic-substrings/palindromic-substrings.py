ad60649ae222d155bc15bf716f7415e003078896
hemanthgowda08 <hemanthkriss007@gmail.com>
2026-06-27 23:37:17 +0530

    Time: 163 ms (78.08%) | Memory: 12.5 MB (38.39%) - LeetSync

---
+++ 647-palindromic-substrings/palindromic-substrings.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def countSubstrings(self, s):
+        count = 0
+
+        for i in range(len(s)):
+
+            left = i
+            right = i
+
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                count += 1
+                left -= 1
+                right += 1
+
+            left = i
+            right = i + 1
+
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                count += 1
+                left -= 1
+                right += 1
+
+        return count
