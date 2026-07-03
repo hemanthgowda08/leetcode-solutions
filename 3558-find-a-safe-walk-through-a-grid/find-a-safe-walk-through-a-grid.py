d37e1a8264d7f08da3b39ceb2eaf583d84368b77
hemanthgowda08 <hemanthkriss007@gmail.com>
2026-07-03 17:58:07 +0530

    Time: 330 ms (44.44%) | Memory: 12.6 MB (55.56%) - LeetSync

--- 3558-find-a-safe-walk-through-a-grid/find-a-safe-walk-through-a-grid.py
+++ 3558-find-a-safe-walk-through-a-grid/find-a-safe-walk-through-a-grid.py
@@ -1,21 +1,19 @@
 from collections import deque
 
-class Solution(object):
+class Solution:
     def findSafeWalk(self, grid, health):
         m = len(grid)
         n = len(grid[0])
 
         health -= grid[0][0]
-
         if health <= 0:
             return False
 
         q = deque([(0, 0, health)])
-
         best = [[-1] * n for _ in range(m)]
         best[0][0] = health
 
-        directions = [(1,0), (-1,0), (0,1), (0,-1)]
+        directions = [(1,0),(-1,0),(0,1),(0,-1)]
 
         while q:
             x, y, h = q.popleft()
@@ -24,8 +22,7 @@ class Solution(object):
                 return True
 
             for dx, dy in directions:
-                nx = x + dx
-                ny = y + dy
+                nx, ny = x + dx, y + dy
 
                 if 0 <= nx < m and 0 <= ny < n:
                     nh = h - grid[nx][ny]
