2dea0a814b1d36cd04e35cdc42cd29e7f99e3946
hemanthgowda08 <hemanthkriss007@gmail.com>
2026-07-12 14:46:31 +0530

    Time: 46 ms (56.79%) | Memory: 27.8 MB (94.24%) - LeetSync

---
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def arrayRankTransform(self, arr):
+        sorted_arr = sorted(set(arr))
+
+        rank = {}
+
+        for i, num in enumerate(sorted_arr) :
+            rank[num] = i + 1
+
+        ans = []
+
+        for num in arr :
+            ans.append(rank[num])
+
+        return ans
+
+            
+
+
+      
