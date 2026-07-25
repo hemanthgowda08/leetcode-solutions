b9549315ffb40a00581fca07628fda3da654d2e9
hemanthgowda08 <hemanthkriss007@gmail.com>
2026-07-25 23:09:17 +0530

    Time: 17 ms (14.43%) | Memory: 12.6 MB (20.81%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -1,6 +1,6 @@
 class Solution(object):
     def romanToInt(self, s):
-        roman = {
+        letters = {
             "I": 1,
             "V": 5,
             "X": 10,
@@ -8,16 +8,16 @@ class Solution(object):
             "C": 100,
             "D": 500,
             "M": 1000
+
         }
+        
+        n = len(s)
+        res = 0
 
-        total = 0
-
-        for i in range(len(s) - 1):
-            if roman[s[i]] < roman[s[i + 1]]:
-                total -= roman[s[i]]
+        for i in range(0,n):
+            if i < n - 1 and letters[s[i]] < letters[s[i + 1]]:
+                res -= letters[s[i]]
             else:
-                total += roman[s[i]]
+                res += letters[s[i]]
 
-        total += roman[s[-1]]
-
-        return total
+        return res
